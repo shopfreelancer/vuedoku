ef3d0ae7d61d071804840e57c77c029a820ae75f
Hans-Christian Psaar <unkult@southofheaven.local>
2017-11-30 20:15:21 +0700

    added error validation for input field.

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,9 +1,9 @@
 <template>
 <div>
     <div class="board">
-        <div class="square" v-for="(field, key) in fields" :key="field.id" v-bind:id="field.id" data-key="field.id">
-            <span data-key="key" v-if="field.value != 0" class="puzzle-field-solution">{{field.value}}</span>
-            <span data-key="key" v-else contenteditable="true" class="puzzle-field-empty"></span>    
+        <div class="square" v-for="(field, key) in fields" :key="field.id" v-bind:id="field.id">
+            <span v-if="field.value != 0" class="puzzle-field-solution">{{field.value}}</span>
+            <input v-else type="number" maxlength="1" min="1" max="9" step="1" v-bind:timeout="field.timeout" @input="validateField(field,$event)" class="puzzle-field-empty">    
         </div>
     </div>
 </div>
@@ -15,12 +15,66 @@ import { PuzzlesStore } from '../stores/PuzzlesStore.js'
 export default {
   name: 'Board',
   created(){
+    
       this.selectRandomPuzzle();
       
       this.generateFields();
       this.assignPuzzleValuesToFields();
   },
   methods: {
+    validateField(field,event){
+        var self = this;
+        
+        field.timeout = setTimeout(function () {
+                    
+            let userInput = parseInt(event.target.value);
+            
+            if(userInput === 2){
+                
+                var errorClass = " has-error";
+                var parentEl = event.target.parentNode;
+                var hasErrorClass = parentEl.className.search(errorClass);
+ 
+                if(!self.hasInputFieldErrorClass(event)){
+                    self.attachErrorClassToInputField(event);  
+                }
+            } else {
+            
+                if(self.hasInputFieldErrorClass(event)){
+                    self.removeErrorClassFromInputField(event);  
+                }
+            }
+
+            if(field.timeout !== "undefined"){
+                clearTimeout(field.timeout);
+            }
+            }, 1000);
+        return;
+        
+    },
+    hasInputFieldErrorClass(event){
+            var errorClass = " has-error";
+            var parentEl = event.target.parentNode;
+            var hasErrorClass = parentEl.className.search(errorClass);
+            
+            if(hasErrorClass === -1){
+                return false;
+            }
+        return true;
+    },
+    attachErrorClassToInputField(event){
+            var errorClass = " has-error";
+            var parentEl = event.target.parentNode;
+            
+            parentEl.className = parentEl.className + errorClass;
+    },
+    removeErrorClassFromInputField(event){
+            var errorClass = " has-error";
+            var parentEl = event.target.parentNode;
+        
+            let classNameWithoutErrorClass = parentEl.className.substr(0,parentEl.className.search(errorClass));
+            parentEl.className = classNameWithoutErrorClass;
+    },
     selectRandomPuzzle(){
         let randomPuzzleId = this.getRandomInt(0,this.puzzles.length);
         this.activePuzzle = this.puzzles[randomPuzzleId];
@@ -46,6 +100,7 @@ export default {
             field.regionIndex = this.calculateRegionIndexForField(field.colIndex,field.rowIndex)
             field.value = 0;
             field.id = i-1;
+            field.timeout = 0;
           
             this.fields.push(field);
         }
@@ -149,6 +204,8 @@ export default {
         min-height: 6vw;
         font-size: 6vw;
         line-height: 6vw;
+        
+        vertical-align:middle;
    
     }
     
@@ -170,8 +227,22 @@ export default {
     
     .puzzle-field-solution {
         color:bisque;
+        vertical-align:middle;
     }
-    .puzzle-field-empty {
+    input.puzzle-field-empty {
         color:#fff;
-    }    
+        width:100%;
+        height:100%;
+        border:0;
+        background-color:transparent;
+        font-size: 6vw;
+        text-align: right;
+        padding:0;
+    }
+    *:focus {
+        outline: none;
+    }
+    .square.has-error {
+        background-color:#ff6300;
+    }
 </style>
